818f92fc5707f768d1d3cf364a7fd737739b0034
TimothéeLVB <timLvb@users.noreply.github.com>
2021-02-18 14:59:44 +0100

    Update ScreenList.js

--- BeABetterYou/Screens/ScreenList.js
+++ BeABetterYou/Screens/ScreenList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import CustomButton from '../utils/Button';
 
 export default function ScreenList({ navigation }) {
